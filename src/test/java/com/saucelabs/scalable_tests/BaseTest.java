e09ca31d07f4c1f1f50c0a0b4e2d6611fc251ac4
titusfortner <titus.fortner@gmail.com>
2023-06-28 20:52:36 -0500

    run in parallel on Sauce Labs

--- src/test/java/com/saucelabs/scalable_tests/BaseTest.java
+++ src/test/java/com/saucelabs/scalable_tests/BaseTest.java
@@ -1,30 +1,71 @@
 package com.saucelabs.scalable_tests;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class BaseTest {
-    WebDriver driver = null;
+    WebDriver driver;
 
     @RegisterExtension
     public MyTestWatcher myTestWatcher = new MyTestWatcher();
 
+    @BeforeAll
+    public static void toggleExecution() {
+        // This would normally be toggled via CI tool ENV or similar
+        System.setProperty("SELENIUM_PLATFORM", "SAUCE");
+    }
+
     @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();
+    public void setUp(TestInfo testinfo) throws MalformedURLException {
+        driver = isLocal() ? runLocal() : runSauce(testinfo);
+    }
+
+    public boolean isLocal() {
+        return "LOCAL".equals(System.getProperty("SELENIUM_PLATFORM"));
+    }
+
+    private WebDriver runLocal() {
+        return new ChromeDriver();
+    }
+
+    private WebDriver runSauce(TestInfo testinfo) throws MalformedURLException {
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("latest");
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.put("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+        sauceOptions.put("name", testinfo.getDisplayName());
+        browserOptions.setCapability("sauce:options", sauceOptions);
+
+        URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+        return new RemoteWebDriver(url, browserOptions);
     }
 
     public class MyTestWatcher implements TestWatcher {
         @Override
         public void testFailed(ExtensionContext context, Throwable cause) {
             try {
-                System.out.println("Test Failed!");
+                if (isLocal()) {
+                    System.out.println("Test Failed!");
+                } else {
+                    ((JavascriptExecutor) driver).executeScript("sauce:job-result=failed");
+                }
             } catch (Exception ignored) {
             } finally {
                 driver.quit();
@@ -34,7 +75,11 @@ public class BaseTest {
         @Override
         public void testSuccessful(ExtensionContext context) {
             try {
-                System.out.println("Test Passed!");
+                if (isLocal()) {
+                    System.out.println("Test Passed!");
+                } else {
+                    ((JavascriptExecutor) driver).executeScript("sauce:job-result=passed");
+                }
             } catch (Exception ignored) {
             } finally {
                 driver.quit();
